909f0729177edf2575bd25714eb6470081343ebb
surya2dot0 <suryap1802@gmail.com>
2023-05-05 18:50:26 +0530

    Project structure for Service Registry

---
+++ ServiceRegistryy/src/main/java/in/learning/ServiceRegistryyApplication.java
@@ -0,0 +1,13 @@
+package in.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryyApplication.class, args);
+	}
+
+}
